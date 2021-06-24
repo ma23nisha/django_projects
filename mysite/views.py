dffb516076d1b17adece3220102990cd97f0f137
manisha <manishasharma.agt@gmail.com>
2021-06-24 16:41:23 +0530

    text utils changes

--- mysite/views.py
+++ mysite/views.py
@@ -5,20 +5,58 @@ def index(request):
     return render(request,'index.html')
 
 def about(request):
-    return HttpResponse("<h1>Hello</h1>")
+    return render(request,'index.html')
 
 def analyze(request):
     text = request.GET.get('text','default')
     removepunc = request.GET.get('removepunc','off')
-    print(removepunc)
-    print(text)
-    if removepunc=="on":
+    fullcaps = request.GET.get('fullcaps','off')
+    newlineremove = request.GET.get('newlineremove','off')
+    removespace = request.GET.get('removespace','off')
+    charcount = request.GET.get('charcount','off')
+
+    if removepunc == "on":
         Punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
-        analyzed=""
+        analyzed = ""
         for char in text:
             if char not in Punctuations:
                 analyzed = analyzed + char
         params = {'purpose':'Removed Punctuations','analyzed_text':analyzed}
+        return render(request,'analyze.html',params)
+       
+
+    if(fullcaps == "on"):
+        analyzed = ""
+        for char in text:
+            analyzed = analyzed + char.upper()
+        params = {'purpose':'Changed to Upper','analyzed_text':analyzed}
+        return render(request,'analyze.html',params)
+    
+
+    if(removespace == "on"):
+        analyzed = ""
+        for index,char  in enumerate(text):
+            if char!=" ":
+                analyzed = analyzed + char.upper()
+        params = {'purpose':'Changed to Upper','analyzed_text':analyzed}
+        return render(request,'analyze.html',params)
+
+
+    if(charcount == "on"):
+        count=0
+        for char in text:
+            count+=1
+        params = {'purpose':'character count','analyzed_text':count}
+        return render(request,'analyze.html',params)
+            
+    if(newlineremove == "on"):
+        analyzed = ""
+        for char in text:
+            if char != "\n":
+                analyzed = analyzed + char.upper()
+        params = {'purpose':'remove newline','analyzed_text':analyzed}
+        return render(request,'analyze.html',params)
+
     else:
         return HttpResponse("Error")
         
